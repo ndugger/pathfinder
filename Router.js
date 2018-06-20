f0324ca82d5b8b90e2b48d3973a4d1f3e02331e8
Nick Dugger <email@nickdugger.com>
2018-06-19 21:24:42 -0500

    Update Router.js

--- Router.js
+++ Router.js
@@ -1,131 +1,99 @@
-module.exports = class Router {
-
-}
-
-// The internal store for routes
-const routes = [];
-
-// The pattern for matching params in paths -- { x }
-const paramsPattern = /\{(.*)\}/;
-
-function exists (method, path) {
-    return false; // TODO
-}
-
-// Supported HTTP methods
 const DELETE = 'DELETE';
 const GET = 'GET';
 const POST = 'POST';
 const PUT = 'PUT';
 
-// Register a route
-function register (method, path, middleware, action) {
-    // Optional trailing slash and remove spaces
-    path = (path.replace(/\/$/, '') || '/').replace(/\s/g, '');
-
-    // Route already exists
-    if (exists(method, path)) {
-        throw new Error(`Route already exists for ${ method } ${ path }`);
+module.exports = class Router {
+    
+    constructor () {
+        this.routes = new Set();
+    }
+    
+    exists (method, uri) {
+        
+        const path = (uri.replace(/\/$/, '') || '/').replace(/\s/g, '');
+        return false; // TODO
     }
 
-    // Route has middleware
-    if (Array.isArray(middleware) && typeof action === 'function') {
-        return routes.push({ 
-            method, 
-            path, 
-            action: (...args) => {
-                return Promise.all(middleware.map(fn => fn(...args))).then(() => action(...args));
-            }
-        });
-    }
-
-    // No middleware, just register action
-    if (!action && typeof middleware === 'function') {
-        return routes.push({ method, path, action: middleware });
-    }
-}
-
-// Shorthand HTTP method wrappers
-const del = register.bind(undefined, DELETE);
-const get = register.bind(undefined, GET);
-const post = register.bind(undefined, POST);
-const put = register.bind(undefined, PUT);
-
-// Shorthand for grouping routes under a parent path
-function group (parent) {
-    return {
-        del (path, action) { del(parent + path, action); return this },
-        get (path, action) { get(parent + path, action); return this },
-        post (path, action) { post(parent + path, action); return this },
-        put (path, action) { put(parent + path, action); return this },
-        group(path) { return group(parent + path) }
-    }
-}
-
-// Find a route based on the request method & the path
-function find (method, path) {
-    try {
-        // Optional trailing slash
-        path = path.replace(/\/$/, '') || '/';
-
-        // Store the found params for later use
-        const params = {};
-
-        // Is the route simple; contains no params?
-        const simpleRoute = routes.find(route => route.method === method && route.path === path);
-
-        if (simpleRoute) {
-            return { params, ...simpleRoute };
+    register (method, uri, middleware, action) {
+        const path = (uri.replace(/\/$/, '') || '/').replace(/\s/g, '');
+        
+        if (this.exists(method, path)) {
+            throw new Error(`Route already exists for ${ method } ${ path }`);
         }
-
-        // Try to find a complex route with params;
-        const complexRoute = routes
-            .filter(route => route.method === method)
-            .filter(route => route.path.split('/').length === path.split('/').length)
-            .find(route => route.path.split('/').every((dir, i) => {
-                // Dir is a param; retrieve value
-                if (dir.match(paramsPattern)) {
-                    params[dir.match(paramsPattern)[1]] = path.split('/')[i];
-                    return true;
+        
+        if (Array.isArray(middleware) && typeof action === 'function') {
+            return routes.add({ 
+                method, 
+                path, 
+                action: (...args) => {
+                    return Promise.all(middleware.map(fn => fn(...args))).then(() => action(...args));
                 }
+            });
+        }
+        
+        if (!action && typeof middleware === 'function') {
+            return this.routes.add({ method, path, action: middleware });
+        }
+    }
+    
+    delete (uri, middleware, action) {
+        return this.register(DELETE, uri, middleware, action);
+    }
+    
+    get (uri, middleware, action) {
+        return this.register(GET, uri, middleware, action);
+    }
+    
+    post (uri, middleware, action) {
+        return this.register(POST, uri, middleware, action);
+    }
+    
+    put (uri, middleware, action) {
+        return this.register(PUT, uri, middleware, action);
+    }
+    
+    find (method, uri) {
+        const routes = Array.from(this.routes);
+        const path = (uri.replace(/\/$/, '') || '/').replace(/\s/g, '');
+        const params = { };
+        
+        try {
+            const simpleRoute = routes.find(route => route.method === method && route.path === path);
 
-                // Dir is just a string and matches request
-                if (dir === path.split('/')[i]) {
-                    return true;
-                }
+            if (simpleRoute) {
+                return { params, ...simpleRoute };
+            }
+            
+            const complexRoute = routes
+                .filter(route => route.method === method)
+                .filter(route => route.path.split('/').length === path.split('/').length)
+                .find(route => route.path.split('/').every((dir, i) => {
+                    
+                    if (dir.match(module.exports.pattern)) {
+                        params[ dir.match(module.exports.pattern)[ 1 ] ] = path.split('/')[ i ];
+                        return true;
+                    }
+                    
+                    if (dir === path.split('/')[ i ]) {
+                        return true;
+                    }
+                    
+                    Object.keys(params).forEach(key => delete params[ key ]);
+                    
+                    return false;
+                }));
+            
+            if (!complexRoute) {
+                return null;
+            }
 
-                // Reset params, wrong route
-                Object.keys(params).forEach(key => delete params[key]);
-                return false;
-            }));
-
-        // No route was found
-        if (!complexRoute) {
+            return { params, ...complexRoute };
+        }
+        catch (e) {
             return null;
         }
-
-        return { params, ...complexRoute };
     }
-    catch (e) {
-        return null;
-    }
-}
+};
 
-export default Object.freeze({
-    
-    DELETE,
-    GET,
-    POST,
-    PUT,
-    
-    register,
-    
-    del,
-    get,
-    post,
-    put,
-    
-    group,
-    
-    find
-});
+module.exports.pattern = /\{(.*)\}/;
